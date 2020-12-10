0d1d23d54d56c8820edfcac7c9a345f0c1005ec4
tmadurell <madurell95>
2020-12-10 04:03:12 +0100

    MENU DE EXERCICIS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
-import java.io.*;
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         new MenuExercicis().mostrar();
 
     }
---
+++ src/com/company/MenuExercicis.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MenuExercicis {
+    public void mostrar() throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        int opcio;
+
+        do {
+
+            System.out.println("");
+            System.out.println("---------------------------------------------------------");
+            System.out.println("MP09-UF1 A5 Keystore,KeyPair & WrappedKey:");
+            System.out.println("Exercicis:\n");
+            System.out.println(" 1.Parell de claus, KeyStore i PublicKey");
+            System.out.println(" 2.Clau embolcallada (1.2.3)");
+            System.out.println(" 3.Sortir");
+            System.out.println("---------------------------------------------------------");
+            System.out.print("  Escriu aquí: ");
+
+
+            opcio = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcio) {
+                case 1:
+                    new EX1().mostrar();
+                    break;
+                case 2:
+                    new EX2().mostrar();
+                    break;
+                case 3:
+                    System.exit(0);
+                    return;
+                default:
+                    System.out.println("Opció no valida");
+                    System.out.println("Solament pots escriure un número del 1-3");
+                    break;
+            }
+        } while (opcio != 3);
+    }
+}
